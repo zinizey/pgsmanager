97caacef037bcc4b14fa1282adec7ebc69ec731c
zinizey <zinireis@gmail.com>
2026-05-12 21:53:27 -0300

    Add database connection setup

---
+++ server/db/connection.js
@@ -0,0 +1,16 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+export default pool;
